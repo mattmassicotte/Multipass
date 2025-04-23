29b9e262b9614d67219861b7e0939fbd355fd308
Matt <85322+mattmassicotte@users.noreply.github.com>
2025-04-23 13:19:54 -0400

    Move view model out to a separate file

--- Modules/Timeline/FeedView.swift
+++ Modules/Timeline/FeedView.swift
@@ -1,102 +1,18 @@
 import SwiftUI
 
 import CompositeSocialService
-import OAuthenticator
 import StableView
 import Storage
 import UIUtility
 
-@frozen
-public enum ViewState<T> {
-  case initial
-  case error
-  case loading
-  case loaded(T)
-}
-
-@MainActor
-@Observable
-final class ViewModel {
-	@ObservationIgnored
-	private var client: CompositeClient
-	@ObservationIgnored
-	private var services: [any SocialService] = []
-	// this is needed to workaround a bug in Xcode 16.3, but my assumption is it will be resolved shortly.
-	#if targetEnvironment(simulator)
-	@ObservationIgnored
-	private let responseProvider = URLSession(configuration: .ephemeral).responseProvider
-	#else
-	@ObservationIgnored
-	private let responseProvider = URLSession.defaultProvider
-	#endif
-	@ObservationIgnored
-	private let secretStore: SecretStore
-
-	private(set) var accountsIdentifier: Int
-	
-	private(set) var posts: [Post] = []
-
-	init(secretStore: SecretStore) {
-		self.secretStore = secretStore
-		self.client = CompositeClient(
-			secretStore: secretStore,
-			services: []
-		)
-		self.accountsIdentifier = 0
-	}
-	
-	func refresh() async {
-		do {
-			self.posts = try await client.timeline().sorted(by: { $0 > $1 })
-		} catch {
-			print("dammm", error)
-		}
-	}
-	
-	func updateAccounts(_ accounts: [UserAccount]) {
-		let services = accounts.map { (account) -> any SocialService in
-			switch account.source {
-			case .mastodon:
-				MastodonService(
-					with: responseProvider,
-					host: account.details.host,
-					secretStore: secretStore
-				)
-			case .bluesky:
-				BlueskyService(
-					with: responseProvider,
-					authServer: account.details.host,
-					account: account.details.user,
-					secretStore: secretStore
-				)
-			}
-		}
-		
-		self.client = CompositeClient(secretStore: secretStore, services: services)
-		self.accountsIdentifier = accounts.hashValue
-	}
-	
-	func handlePostAction(action: PostStatusAction, post: Post) {
-		switch action {
-		case .like:
-			Task {
-				try! await self.client.likePost(post)
-			}
-		case .repost:
-			print("nope, not yet")
-		}
-		
-	}
-}
-
 public struct FeedView: View {
-	@State private var model: ViewModel
+	@State private var model: FeedViewModel
 	@State private var scrollState: AnchoredListPosition<Post> = .absolute(0.0)
 	@Environment(UserAccountStore.self) private var accountStore
 	@Environment(\.openURL) private var openURL
 	
 	public init(secretStore: SecretStore) {
-		self._model = State(wrappedValue: ViewModel(secretStore: secretStore))
+		self._model = State(wrappedValue: FeedViewModel(secretStore: secretStore))
 	}
 	
 	public var body: some View {
